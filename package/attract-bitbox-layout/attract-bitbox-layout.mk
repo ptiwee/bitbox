74426d00fa9a004a351ffd3d49e0c8a1ed5c5cf9
Baptiste Reynal <baptiste.reynal@hap2u.net>
2022-07-19 13:57:57 +0200

    Add attract-bitbox-layout package

---
+++ package/attract-bitbox-layout/attract-bitbox-layout.mk
@@ -0,0 +1,10 @@
+ATTRACT_BITBOX_LAYOUT_VERSION = 3e0d54e
+ATTRACT_BITBOX_LAYOUT_SITE = https://github.com/ptiwee/attract-bitbox
+ATTRACT_BITBOX_LAYOUT_SITE_METHOD = git
+
+define ATTRACT_BITBOX_LAYOUT_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/attract/layouts/BitBox
+	cp -r $(@D)/* $(TARGET_DIR)/usr/share/attract/layouts/BitBox/
+endef
+
+$(eval $(generic-package))
