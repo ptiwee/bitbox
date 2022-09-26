7efb07c4e27ad3b6479707c37d7f132263cbefee
Baptiste Reynal <baptiste.reynal@hap2u.net>
2022-09-26 13:57:27 +0200

    Add extra emulators

---
+++ package/libretro-beetle-ngp/libretro-beetle-ngp.mk
@@ -0,0 +1,16 @@
+LIBRETRO_BEETLE_NGP_VERSION = 00c7cb8
+LIBRETRO_BEETLE_NGP_SITE = https://github.com/libretro/beetle-ngp-libretro
+LIBRETRO_BEETLE_NGP_SITE_METHOD = git
+LIBRETRO_BEETLE_NGP_DEPENDENCIES = retroarch
+
+define LIBRETRO_BEETLE_NGP_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) \
+        -C $(@D) platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_BEETLE_NGP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/mednafen_ngp_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/libretro_beetle_ngp.so
+endef
+
+$(eval $(generic-package))
