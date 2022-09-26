39069a9e7781c3661ab52264c0e2533d06f54eb5
Baptiste Reynal <baptiste.reynal@hap2u.net>
2022-09-26 16:16:35 +0200

    Fix bbconfigs path

--- package/bbconfigs/bbconfigs.mk
+++ package/bbconfigs/bbconfigs.mk
@@ -1,6 +1,6 @@
 define BBINIT_INSTALL_TARGET_CMDS
-	cp -r $(BR2_EXTERNAL)/package/bbinit/attract $(TARGET_DIR)/usr/share/
-	cp -r $(BR2_EXTERNAL)/package/bbinit/retroarch $(TARGET_DIR)/usr/share/
+	cp -r $(BR2_EXTERNAL)/package/bbconfigs/attract $(TARGET_DIR)/usr/share/
+	cp -r $(BR2_EXTERNAL)/package/bbconfigs/retroarch $(TARGET_DIR)/usr/share/
 endef
 
 $(eval $(generic-package))
