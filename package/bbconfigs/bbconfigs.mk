b0b3b36616ff2327b93213b34232c42574c811a3
Baptiste Reynal <baptiste.reynal@hap2u.net>
2022-09-28 17:55:57 +0200

    Add default config

--- package/bbconfigs/bbconfigs.mk
+++ package/bbconfigs/bbconfigs.mk
@@ -1,4 +1,4 @@
-define BBINIT_INSTALL_TARGET_CMDS
+define BBCONFIGS_INSTALL_TARGET_CMDS
 	cp -r $(BR2_EXTERNAL)/package/bbconfigs/attract $(TARGET_DIR)/usr/share/
 	cp -r $(BR2_EXTERNAL)/package/bbconfigs/retroarch $(TARGET_DIR)/usr/share/
 endef
